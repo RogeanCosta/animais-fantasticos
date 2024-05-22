da61b7b3a36952a7d0a18817a3291a7cd88c23e5
Rogean Costa <rogean.ifce@gmail.com>
2024-05-22 15:20:58 -0300

    Animação a navegação por scroll

--- script.js
+++ script.js
@@ -67,3 +67,29 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+
+        if (isSectionVisible) {
+          section.classList.add(activeClass);
+        } else {
+          section.classList.remove(activeClass);
+        }
+      });
+    }
+
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll();
